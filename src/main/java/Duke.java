a229a340bf436eea23fbc8199cf79632645728d6
tianqi77 <e0323090@u.nus.edu>
2020-01-29 23:07:44 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.FileOutputStream;
-
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) throws Exception{
         String logo = " ____        _        \n"
@@ -11,14 +11,23 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.print("Hello! I'm Duke\n" + "What can I do for you?\n");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
                 java.io.FileDescriptor.out)));
+        out.write("Hello from\n" + logo);
+        out.write("Hello! I'm Duke\n" + "What can I do for you?\n");
+        out.flush();
         String cmd = br.readLine();
+        ArrayList<String> strings = new ArrayList<>(100);
         while(!cmd.equals("bye")) {
-            out.write(cmd + '\n');
+            if(cmd.equals("list")){
+                for(int i = 0; i < strings.size(); i++){
+                    out.write(String.valueOf(i+1) + ". " + strings.get(i) + "\n");
+                }
+            }else {
+                strings.add(cmd);
+                out.write("added:" + cmd + '\n');
+            }
             out.flush();
             cmd = br.readLine();
         }
