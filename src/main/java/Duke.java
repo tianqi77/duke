2d87d6b81555040515bc7895e1407c7bc60d1914
tianqi77 <e0323090@u.nus.edu>
2020-01-30 02:05:10 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,27 +19,37 @@ public class Duke {
         out.write("Hello from\n" + logo);
         out.write("Hello! I'm Duke\n" + "What can I do for you?\n");
         out.flush();
-        StringTokenizer st  = new StringTokenizer(br.readLine());
+        String curr = br.readLine();
+        StringTokenizer st  = new StringTokenizer(curr);
         String cmd = st.nextToken();
         ArrayList<Task> tasks = new ArrayList<>(100);
+        Task currTask = null;
         while(!cmd.equals("bye")) {
             if(cmd.equals("list")){
                 out.write("Here are the tasks in your list:\n");
                 for(int i = 0; i < tasks.size(); i++){
-                    out.write(String.valueOf(i+1) + "." +  tasks.get(i).status
-                            + tasks.get(i).name + "\n");
+                    out.write(String.valueOf(i+1) + ".");
+                    tasks.get(i).print(out);
                 }
             }else if(cmd.equals("done")){
                 int idx = Integer.parseInt(st.nextToken())-1;
-                tasks.get(idx).done();
-                out.write("Nice! I've marked this task as done:\n");
-                out.write("  " + tasks.get(idx).status +  tasks.get(idx).name + "\n");
+                tasks.get(idx).done(out);
             }else {
-                tasks.add(new Task(cmd));
-                out.write("added:" + cmd + '\n');
+                if (cmd.equals("todo")) {
+                    currTask = new Task(curr.substring(5));
+                } else if (cmd.equals("deadline")) {
+                    int endIdx = curr.indexOf(" /by");
+                    currTask = new Deadline(curr.substring(9, endIdx), " (by:" + curr.substring(endIdx + 4) + ")");
+                } else if (cmd.equals("event")) {
+                    int endIdx = curr.indexOf(" /at");
+                    currTask = new Event(curr.substring(6, endIdx), " (at:" + curr.substring(endIdx + 4) + ")");
+                }
+                tasks.add(currTask);
+                currTask.addTask(out, tasks.size());
             }
             out.flush();
-            st = new StringTokenizer(br.readLine());
+            curr = br.readLine();
+            st = new StringTokenizer(curr);
             cmd = st.nextToken();
         }
         out.write("Bye. Hope to see you again soon!\n");
@@ -49,12 +59,52 @@ public class Duke {
 }
 
 class Task{
-    public String status = "[" + "\u2717" +"] " ;
+    public String type = "[T]";
+    public String status = "[" + "\u2718" +"] " ;
     public String name;
     public Task(String x){
         this.name = x;
     }
-    public void done(){
+    public void print(BufferedWriter out) throws Exception{
+        out.write(this.type + this.status + this.name + "\n");
+    }
+    public void done(BufferedWriter out) throws Exception{
         this.status ="[" + "\u2713" +"] " ;
+        out.write("Nice! I've marked this task as done:\n" + "  ");
+        this.print(out);
+    }
+    public void addTask(BufferedWriter out, int number) throws Exception {
+        out.write("Got it. I've added this task:\n"+"  ");
+        this.print(out);
+        if(number > 1) {
+            out.write("Now you have " + String.valueOf(number) + " tasks in the list\n");
+        }else{
+            out.write("Now you have 1 task in the list\n");
+        }
+    }
+}
+class Deadline extends Task{
+    public String ddl;
+    public Deadline (String x, String d){
+        super(x);
+        this.type = "[D]";
+        this.ddl = d;
+    }
+    @Override
+    public void print(BufferedWriter out) throws Exception{
+        out.write(this.type + this.status + this.name + this.ddl + "\n");
+    }
+
+}
+class Event extends Task{
+    public String venue;
+    public Event(String x, String v){
+        super(x);
+        this.type = "[E]";
+        this.venue = v;
+    }
+    @Override
+    public void print(BufferedWriter out) throws Exception{
+        out.write(this.type + this.status + this.name + this.venue + "\n");
     }
 }
