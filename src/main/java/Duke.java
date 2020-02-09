57150b9950b8853d7e2baf0df8da373629d43179
tianqi77 <e0323090@u.nus.edu>
2020-02-10 01:20:42 +0800

    JavaFX1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,16 +9,24 @@ import java.io.IOException;
 
 import java.nio.charset.StandardCharsets;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * The main class that runs the duke project.
  */
-public class Duke {
+public class Duke extends Application {
     private Storage storage;
     private Ui ui;
     private TaskList tasks;
     private BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
             java.io.FileDescriptor.out), StandardCharsets.UTF_8));
 
+    /*public Duke() {
+
+    }*/
     /**
      * Constructor of an instance of Duke class.
      * It creates instances of Ui, Storage, and TaskList for processing.
@@ -56,6 +64,15 @@ public class Duke {
         }
         br.close();
         out.close();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     public static void main(String[] args) throws Exception {
