0755d051ba1b43dd7a51a0e71dc5f91f9a19681a
tianqi77 <e0323090@u.nus.edu>
2020-02-10 01:24:44 +0800

    Revert "JavaFX1"
    This reverts commit 57150b9950b8853d7e2baf0df8da373629d43179.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,24 +9,16 @@ import java.io.IOException;
 
 import java.nio.charset.StandardCharsets;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
 /**
  * The main class that runs the duke project.
  */
-public class Duke extends Application {
+public class Duke {
     private Storage storage;
     private Ui ui;
     private TaskList tasks;
     private BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
             java.io.FileDescriptor.out), StandardCharsets.UTF_8));
 
-    /*public Duke() {
-
-    }*/
     /**
      * Constructor of an instance of Duke class.
      * It creates instances of Ui, Storage, and TaskList for processing.
@@ -64,15 +56,6 @@ public class Duke extends Application {
         }
         br.close();
         out.close();
-    }
-
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
     }
 
     public static void main(String[] args) throws Exception {
